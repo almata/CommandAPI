bb51d34ec7106a4133eb16c32cb72c33f49799b7
Albert Mata <hello@albertmata.net>
2021-02-20 07:59:38 +0100

    Add real (not mock) repository connected to DB

---
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext context;
+
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return context.CommandItems.ToList();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return context.CommandItems.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
